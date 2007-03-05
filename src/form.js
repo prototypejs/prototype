a9ec09115b68318f4e656f6a46b3df6271b47dbb
Thomas Fuchs <thomas@fesch.at>
2007-03-05 16:03:11 +0000

    Prototype: Form.Element.disable() will now call blur(), removed blur() call from Form.Element.enable().  Closes #6034.  [tdd]

--- src/form.js
+++ src/form.js
@@ -160,13 +160,13 @@ Form.Element.Methods = {
   
   disable: function(element) {
     element = $(element);
+    element.blur();
     element.disabled = true;
     return element;
   },
   
   enable: function(element) {
     element = $(element);
-    element.blur();
     element.disabled = false;
     return element;
   }
