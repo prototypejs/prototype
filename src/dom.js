b76d34e7bb1f952fa1f3cfa2f8a4de5bfc4ab2be
Sam Stephenson <sam@37signals.com>
2007-04-28 02:32:21 +0000

    prototype: Fix regression in which no-argument calls to Element.addMethods() would not add the methods defined in Form.Methods and Form.Element.Methods.  Closes #8206.

--- src/dom.js
+++ src/dom.js
@@ -586,6 +586,18 @@ Element.hasAttribute = function(element, attribute) {
 
 Element.addMethods = function(methods) {
   var F = Prototype.BrowserFeatures, T = Element.Methods.ByTag;
+  
+  if (!methods) {
+    Object.extend(Form, Form.Methods);
+    Object.extend(Form.Element, Form.Element.Methods);
+    Object.extend(Element.Methods.ByTag, {
+      "FORM":     Object.clone(Form.Methods),
+      "INPUT":    Object.clone(Form.Element.Methods),
+      "SELECT":   Object.clone(Form.Element.Methods),
+      "TEXTAREA": Object.clone(Form.Element.Methods)
+    });
+  }
+  
   if (arguments.length == 2) {
     var tagName = methods;
     methods = arguments[1];
--- src/form.js
+++ src/form.js
@@ -97,8 +97,6 @@ Form.Methods = {
   }
 }
 
-Object.extend(Form, Form.Methods);
-
 /*--------------------------------------------------------------------------*/
 
 Form.Element = {
@@ -166,14 +164,6 @@ Form.Element.Methods = {
     return element;
   }
 }
-
-Object.extend(Form.Element, Form.Element.Methods);
-Object.extend(Element.Methods.ByTag, {
-  "FORM":     Object.clone(Form.Methods),
-  "INPUT":    Object.clone(Form.Element.Methods),
-  "SELECT":   Object.clone(Form.Element.Methods),
-  "TEXTAREA": Object.clone(Form.Element.Methods)
-});
 
 /*--------------------------------------------------------------------------*/
 
