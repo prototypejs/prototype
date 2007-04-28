b360a93999c4e4ebe31c6f6427416bdb03f8bd80
Sam Stephenson <sam@37signals.com>
2007-04-28 02:37:53 +0000

    prototype: Fix jstest.rb Konqueror support.  Closes #7140.

--- test/lib/jstest.rb
+++ test/lib/jstest.rb
@@ -1,6 +1,8 @@
 require 'rake/tasklib'
 require 'thread'
 require 'webrick'
+require 'fileutils'
+include FileUtils
 
 class Browser
   def supported?; true; end
@@ -100,8 +102,32 @@ class IEBrowser < Browser
 end
 
 class KonquerorBrowser < Browser
+  @@configDir = File.join(ENV['HOME'], '.kde', 'share', 'config')
+  @@globalConfig = File.join(@@configDir, 'kdeglobals')
+  @@konquerorConfig = File.join(@@configDir, 'konquerorrc')
+
   def supported?
     linux?
+  end
+
+  # Forces KDE's default browser to be Konqueror during the tests, and forces
+  # Konqueror to open external URL requests in new tabs instead of a new
+  # window.
+  def setup
+    cd @@configDir, :verbose => false do
+      copy @@globalConfig, "#{@@globalConfig}.bak", :preserve => true, :verbose => false
+      copy @@konquerorConfig, "#{@@konquerorConfig}.bak", :preserve => true, :verbose => false
+      # Too lazy to write it in Ruby...  Is sed dependency so bad?
+      system "sed -ri /^BrowserApplication=/d  '#{@@globalConfig}'"
+      system "sed -ri /^KonquerorTabforExternalURL=/s:false:true: '#{@@konquerorConfig}'"
+    end
+  end
+
+  def teardown
+    cd @@configDir, :verbose => false do
+      copy "#{@@globalConfig}.bak", @@globalConfig, :preserve => true, :verbose => false
+      copy "#{@@konquerorConfig}.bak", @@konquerorConfig, :preserve => true, :verbose => false
+    end
   end
   
   def visit(url)
