5645a0783ff9a87a56c9ecbd501cc6e8fa3d12be
Sam Stephenson <sam@37signals.com>
2007-07-26 02:03:40 +0000

    prototype: Add Prototype.Browser.MobileSafari which evaluates to true on the iPhone's browser.

--- src/prototype.js
+++ src/prototype.js
@@ -7,16 +7,16 @@ var Prototype = {
     IE:     !!(window.attachEvent && !window.opera),
     Opera:  !!window.opera,
     WebKit: navigator.userAgent.indexOf('AppleWebKit/') > -1,
-    Gecko:  navigator.userAgent.indexOf('Gecko') > -1 && navigator.userAgent.indexOf('KHTML') == -1
+    Gecko:  navigator.userAgent.indexOf('Gecko') > -1 && navigator.userAgent.indexOf('KHTML') == -1,
+    MobileSafari: !!navigator.userAgent.match(/iPhone.*Mobile.*Safari/)
   },
 
   BrowserFeatures: {
     XPath: !!document.evaluate,
     ElementExtensions: !!window.HTMLElement,
     SpecificElementExtensions: 
-      navigator.userAgent.indexOf('iPhone') == -1 &&
-      (document.createElement('div').__proto__ !== 
-       document.createElement('form').__proto__)
+      document.createElement('div').__proto__ !== 
+       document.createElement('form').__proto__
   },
 
   ScriptFragment: '<script[^>]*>([\\S\\s]*?)<\/script>',
@@ -24,7 +24,10 @@ var Prototype = {
   
   emptyFunction: function() { },
   K: function(x) { return x }
-}
+};
+
+if (Prototype.Browser.MobileSafari)
+  Prototype.BrowserFeatures.SpecificElementExtensions = false;
 
 <%= include 'base.js', 'string.js' %>
 
