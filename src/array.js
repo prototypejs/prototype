5a37860ffbf97559c75694db3793de30a7370c0b
Tobie Langel <tobie.langel@gmail.com>
2007-12-20 15:47:32 +0000

    prototype: Ensure  creates an empty array when its argument's length is undefined. Closes #10574

--- src/array.js
+++ src/array.js
@@ -1,7 +1,7 @@
 function $A(iterable) {
   if (!iterable) return [];
   if (iterable.toArray) return iterable.toArray();
-  var length = iterable.length, results = new Array(length);
+  var length = iterable.length || 0, results = new Array(length);
   while (length--) results[length] = iterable[length];
   return results;
 }
@@ -11,7 +11,7 @@ if (Prototype.Browser.WebKit) {
     if (!iterable) return [];
     if (!(Object.isFunction(iterable) && iterable == '[object NodeList]') &&
         iterable.toArray) return iterable.toArray();
-    var length = iterable.length, results = new Array(length);
+    var length = iterable.length || 0, results = new Array(length);
     while (length--) results[length] = iterable[length];
     return results;
   }
