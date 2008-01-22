c200c27f480712cc78d5de8e70737883bed6c2c3
Andrew Dupont <prototype@andrewdupont.net>
2008-01-22 23:36:47 +0000

    Fix "function $A" declaration inside of a conditional (confuses IE). Fixes #10882. [Jacco, Andrew Dupont]

--- src/array.js
+++ src/array.js
@@ -7,14 +7,14 @@ function $A(iterable) {
 }
 
 if (Prototype.Browser.WebKit) {
-  function $A(iterable) {
+  $A = function(iterable) {
     if (!iterable) return [];
     if (!(Object.isFunction(iterable) && iterable == '[object NodeList]') &&
         iterable.toArray) return iterable.toArray();
     var length = iterable.length || 0, results = new Array(length);
     while (length--) results[length] = iterable[length];
     return results;
-  }
+  };
 }
 
 Array.from = $A;
