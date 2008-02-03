d770a6c704021e6408186a79abd87214b4fe51d1
Tobie Langel <tobie.langel@gmail.com>
2008-02-03 21:08:13 +0000

    prototype: Prevent Enumerable#eachSlice from entering into an endless loop if passed an argument smaller than 1. Closes #10665.

--- src/enumerable.js
+++ src/enumerable.js
@@ -17,6 +17,7 @@ var Enumerable = {
   eachSlice: function(number, iterator, context) {
     iterator = iterator ? iterator.bind(context) : Prototype.K;
     var index = -number, slices = [], array = this.toArray();
+    if (number < 1) return array;
     while ((index += number) < array.length)
       slices.push(array.slice(index, index+number));
     return slices.collect(iterator, context);
