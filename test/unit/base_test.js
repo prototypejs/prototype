5a1ef1e05c2cc0428f70dc7c95e406b465b268b7
Tobie Langel <tobie.langel@gmail.com>
2008-05-05 16:26:31 +0200

    Fix Function#argumentNames.

--- test/unit/base_test.js
+++ test/unit/base_test.js
@@ -3,6 +3,7 @@ new Test.Unit.Runner({
     this.assertEnumEqual([], (function() {}).argumentNames());
     this.assertEnumEqual(["one"], (function(one) {}).argumentNames());
     this.assertEnumEqual(["one", "two", "three"], (function(one, two, three) {}).argumentNames());
+    this.assertEnumEqual(["one", "two", "three"], (function(  one  , two  , three   ) {}).argumentNames());
     this.assertEqual("$super", (function($super) {}).argumentNames().first());
     
     function named1() {};
