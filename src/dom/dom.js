3977e66796a415234eb0278b077fafad82d63ed2
Andrew Dupont <prototype@andrewdupont.net>
2008-12-12 23:53:39 -0600

    Added Element#store and Element#retrieve for safe, hash-backed storage of element metadata (no memory leaks). Also added Element#getStorage for working with the element's storage hash directly. Hat tip: Mootools.

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1205,3 +1205,41 @@ document.viewport = {
       window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop);
   }
 };
+
+Element.Storage = {
+  UID: 1
+};
+
+Element.addMethods({
+  getStorage: function(element) {
+    if (!(element = $(element))) return;
+    
+    if (Object.isUndefined(element._prototypeUID))
+      element._prototypeUID = [Element.Storage.UID++];
+      
+    var uid = element._prototypeUID[0];
+    
+    if (!Element.Storage[uid])
+      Element.Storage[uid] = $H();
+    
+    return Element.Storage[uid];
+  },
+  
+  store: function(element, key, value) {
+    if (!(element = $(element))) return;
+    element.getStorage().set(key, value);
+  },
+  
+  retrieve: function(element, key, defaultValue) {
+    if (!(element = $(element))) return;
+    
+    var hash = element.getStorage(), value = hash.get(key);
+    
+    if (Object.isUndefined(value)) {
+      hash.set(key, defaultValue);
+      value = defaultValue;
+    }
+    
+    return value;
+  }
+});
