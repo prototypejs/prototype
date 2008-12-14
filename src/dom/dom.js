77b9a2614a90bd758bf0cface597019587103e68
Andrew Dupont <prototype@andrewdupont.net>
2008-12-14 01:35:12 -0600

    Alter element storage API to handle the `window` object gracefully.

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1214,11 +1214,15 @@ Element.addMethods({
   getStorage: function(element) {
     if (!(element = $(element))) return;
     
-    if (Object.isUndefined(element._prototypeUID))
-      element._prototypeUID = [Element.Storage.UID++];
-      
-    var uid = element._prototypeUID[0];
-    
+    var uid;
+    if (element === window) {
+      uid = 0;
+    } else {
+      if (Object.isUndefined(element._prototypeUID))
+        element._prototypeUID = [Element.Storage.UID++];
+      uid = element._prototypeUID[0];
+    }
+        
     if (!Element.Storage[uid])
       Element.Storage[uid] = $H();
     
@@ -1232,8 +1236,7 @@ Element.addMethods({
   
   retrieve: function(element, key, defaultValue) {
     if (!(element = $(element))) return;
-    
-    var hash = element.getStorage(), value = hash.get(key);
+    var hash = Element.getStorage(element), value = hash.get(key);
     
     if (Object.isUndefined(value)) {
       hash.set(key, defaultValue);
