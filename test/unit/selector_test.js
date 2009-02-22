8fe518719e24bf20dca77182e05b3c391a196fa5
Andrew Dupont <prototype@andrewdupont.net>
2009-02-22 15:16:19 -0600

    Fix Selector to match elements with attributes containing hyphens. [#285 state:resolved] (leiyou, jddalton, kangax)

--- test/unit/selector_test.js
+++ test/unit/selector_test.js
@@ -87,6 +87,10 @@ new Test.Unit.Runner({
     this.assertEnumEqual($$('#troubleForm *[type]'), $$('#troubleForm [type]'));
   },
   
+  testSelectorWithAttributeContainingDash: function() {
+    this.assertEnumEqual([$('attr_with_dash')], $$('[foo-bar]'));
+  },                                                            
+  
   testSelectorWithUniversalAndHyphenTokenizedAttributeValue: function() {
     this.assertEnumEqual([$('item_3')], $$('*[xml:lang|="es"]'));
     this.assertEnumEqual([$('item_3')], $$('*[xml:lang|="ES"]'));
