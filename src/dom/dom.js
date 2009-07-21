b57ea83222eadb3996ae4d212823cd7a620ce801
Tobie Langel <tobie.langel@gmail.com>
2009-07-22 01:10:04 +0200

    Remove redundant if statement in Element#readAttribute.

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -596,11 +596,6 @@ Element.Methods = {
   readAttribute: function(element, name) {
     element = $(element);
     if (Prototype.Browser.IE) {
-      // Circumvent issue in IE that causes crash.
-      if (name === 'type' &&
-        element.tagName.toUpperCase() == 'IFRAME') {
-        return element.getAttribute('type');
-      }
       var t = Element._attributeTranslations.read;
       if (t.values[name]) return t.values[name](element, name);
       if (t.names[name]) name = t.names[name];
