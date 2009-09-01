f4ea4c6ef7f48dca963e54fc6203350b2d233b97
Samuel Lebeau <samuel.lebeau@gmail.com>
2009-09-01 15:59:47 +0200

    Rewrite `String#camelize` using `String#replace` with a replacement function [#297 state:resolved]

--- src/lang/string.js
+++ src/lang/string.js
@@ -311,17 +311,9 @@ Object.extend(String.prototype, (function() {
    *      // -> 'MozBinding'
   **/
   function camelize() {
-    var parts = this.split('-'), len = parts.length;
-    if (len == 1) return parts[0];
-
-    var camelized = this.charAt(0) == '-'
-      ? parts[0].charAt(0).toUpperCase() + parts[0].substring(1)
-      : parts[0];
-
-    for (var i = 1; i < len; i++)
-      camelized += parts[i].charAt(0).toUpperCase() + parts[i].substring(1);
-
-    return camelized;
+    return this.replace(/-+(.)?/g, function(match, chr) {
+      return chr ? chr.toUpperCase() : '';
+    });
   }
 
   /**
