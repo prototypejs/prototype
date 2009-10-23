17e8064d8a154b14b97a29d91fc7dd849dff8265
Tobie Langel <tobie.langel@gmail.com>
2009-10-23 06:39:59 +0200

    Add legacy Prototype selector engine. Acessible as Prototype.Legacy. Use SELECTOR_ENGINE=legacy to build.

---
+++ vendor/legacy/selector_engine.js
@@ -0,0 +1,17 @@
+//= require "legacy"
+
+Prototype.Selector = (function(Legacy) {
+  function select(selector, scope) {
+    return Legacy.findChildElements(scope || document, [selector]);
+  }
+  
+  function match(element, selector) {
+    return !!Legacy.findElement([element], selector);
+  }
+  
+  return {
+    select: select,
+    match:  match,
+    filter: Legacy.matchElements
+  };
+})(Prototype.Legacy);
