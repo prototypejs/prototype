6ebfdd51d52c1227d471676586880079511e142d
Andrew Dupont <prototype@andrewdupont.net>
2009-12-04 18:23:29 -0600

    Move a few methods from dom.js to layout.js.

--- src/dom/dom.js
+++ src/dom/dom.js
@@ -1041,35 +1041,6 @@ Element.Methods = {
     return element;
   },
 
-  /**
-   *  Element.getDimensions(@element) -> Object
-   *
-   *  Finds the computed width and height of `element` and returns them as
-   *  key/value pairs of an object.
-  **/
-  getDimensions: function(element) {
-    element = $(element);
-    var display = Element.getStyle(element, 'display');
-    if (display != 'none' && display != null) // Safari bug
-      return {width: element.offsetWidth, height: element.offsetHeight};
-
-    // All *Width and *Height properties give 0 on elements with display none,
-    // so enable the element temporarily
-    var els = element.style;
-    var originalVisibility = els.visibility;
-    var originalPosition = els.position;
-    var originalDisplay = els.display;
-    els.visibility = 'hidden';
-    if (originalPosition != 'fixed') // Switching fixed to absolute causes issues in Safari
-      els.position = 'absolute';
-    els.display = 'block';
-    var originalWidth = element.clientWidth;
-    var originalHeight = element.clientHeight;
-    els.display = originalDisplay;
-    els.position = originalPosition;
-    els.visibility = originalVisibility;
-    return {width: originalWidth, height: originalHeight};
-  },
 
   /**
    *  Element.makePositioned(@element) -> Element
