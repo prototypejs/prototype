2f9bde3ad5a2e3dd104c812b6c81f4077fe0aa1e
John-David Dalton <john.david.dalton@gmail.com>
2010-04-06 14:50:18 -0700

    prototype: Simplify the NWMatcher adapter and optimize it for browsers that don't need to extend DOM elements. [jddalton]

--- vendor/nwmatcher/selector_engine.js
+++ vendor/nwmatcher/selector_engine.js
@@ -2,8 +2,12 @@ Prototype._original_property = window.NW;
 //= require "repository/src/nwmatcher"
 
 Prototype.Selector = (function(engine) {
-  function select(selector, scope) {
-    return engine.select(selector, scope || document, Element.extend);
+  var select = engine.select;
+
+  if (Element.extend !== Prototype.K) {
+    select = function select(selector, scope) {
+      return engine.select(selector, scope, Element.extend);
+    };
   }
 
   return {
