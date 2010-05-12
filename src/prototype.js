0ce5f4fad7cbb35f70d73d2abc187e94e9b9011c
Sam Stephenson <sam@37signals.com>
2010-05-12 11:01:35 -0500

    Update copyright

--- src/prototype.js
+++ src/prototype.js
@@ -1,5 +1,5 @@
 /*  Prototype JavaScript framework, version <%= PROTOTYPE_VERSION %>
- *  (c) 2005-2009 Sam Stephenson
+ *  (c) 2005-2010 Sam Stephenson
  *
  *  Prototype is freely distributable under the terms of an MIT-style license.
  *  For details, see the Prototype web site: http://www.prototypejs.org/
