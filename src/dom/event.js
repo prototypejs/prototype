f71bb853f70eed54fd4b4bbfeb5a8ea72874c9ff
Andrew Dupont <prototype@andrewdupont.net>
2010-10-17 16:09:12 -0500

    Define a `relatedTarget` property on extended mouseenter/mouseleave events in IE's legacy event system. [#708 state:resolved] (Walter Smith, Tobie Langel, Andrew Dupont)

--- src/dom/event.js
+++ src/dom/event.js
@@ -370,9 +370,16 @@
     function _relatedTarget(event) {
       var element;
       switch (event.type) {
-        case 'mouseover': element = event.fromElement; break;
-        case 'mouseout':  element = event.toElement;   break;
-        default: return null;
+        case 'mouseover':
+        case 'mouseenter':
+          element = event.fromElement;
+          break;
+        case 'mouseout':
+        case 'mouseleave':
+          element = event.toElement;
+          break;
+        default:
+          return null;
       }
       return Element.extend(element);
     }
