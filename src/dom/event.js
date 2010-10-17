f6e77f6595c4fe1a71239656f6b7c8ef9bd5a7fe
Andrew Dupont <prototype@andrewdupont.net>
2010-10-17 01:48:46 -0500

    Fix issue with Event#isMiddleClick and #isRightClick on Safari and Chrome. [#731 state:resolved] (Arthur Schreiber)

--- src/dom/event.js
+++ src/dom/event.js
@@ -100,7 +100,8 @@
     _isButton = function(event, code) {
       switch (code) {
         case 0: return event.which == 1 && !event.metaKey;
-        case 1: return event.which == 1 && event.metaKey;
+        case 1: return event.which == 2 || (event.which == 1 && event.metaKey);
+        case 2: return event.which == 3;
         default: return false;
       }
     };
