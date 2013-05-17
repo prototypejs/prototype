658ac2028879e78e3f25aba4b1a1f633187e61b5
Jason Westbrook <jwestbrook@gmail.com>
2013-05-16 20:17:03 -0700

    fixes issue #3284
    https://prototype.lighthouseapp.com/projects/8886/tickets/3284

--- src/prototype/dom/layout.js
+++ src/prototype/dom/layout.js
@@ -1114,8 +1114,8 @@
       }
     } while (element);
     
-    valueL -= layout.get('margin-top');
-    valueT -= layout.get('margin-left');
+    valueT -= layout.get('margin-top');
+    valueL -= layout.get('margin-left');
     
     return new Element.Offset(valueL, valueT);
   }
