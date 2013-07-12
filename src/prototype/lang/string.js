2302e02e939d1d3460851e086e31a63d9fd16edc
Jason Westbrook <jwestbrook@gmail.com>
2013-07-12 15:18:23 -0700

    include perf boost as well as ECMA6 detect for include or contains

--- src/prototype/lang/string.js
+++ src/prototype/lang/string.js
@@ -769,7 +769,7 @@ Object.extend(String.prototype, (function() {
    *      //-> false
   **/
   function include(pattern) {
-    return this.indexOf(pattern) > -1;
+    return this.indexOf(pattern) !== -1;
   }
 
   /**
@@ -910,7 +910,8 @@ Object.extend(String.prototype, (function() {
     unfilterJSON:   unfilterJSON,
     isJSON:         isJSON,
     evalJSON:       NATIVE_JSON_PARSE_SUPPORT ? parseJSON : evalJSON,
-    include:        include,
+    //ECMA 6 supports contains(), if it exists map include() to contains()
+    include:        String.prototype.contains || include,
     // Firefox 18+ supports String.prototype.startsWith, String.prototype.endsWith
     startsWith:     String.prototype.startsWith || startsWith,
     endsWith:       String.prototype.endsWith || endsWith,
