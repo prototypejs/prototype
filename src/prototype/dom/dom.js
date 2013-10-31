0d718c5a39bdadd83c698cb97d4891adf08f7c1d
Jason Westbrook <jwestbrook@gmail.com>
2013-10-30 21:01:28 -0700

    Added fix based on https://prototype.lighthouseapp.com/projects/8886/tickets/3701

--- src/prototype/dom/dom.js
+++ src/prototype/dom/dom.js
@@ -3020,7 +3020,7 @@
 
     var filter = Element.getStyle(element, 'filter');
     if (filter.length === 0) return 1.0;
-    var match = (filter || '').match(/alpha\(opacity=(.*)\)/);
+    var match = (filter || '').match(/alpha\(opacity=(.*)\)/i);
     if (match && match[1]) return parseFloat(match[1]) / 100;
     return 1.0;
   }
