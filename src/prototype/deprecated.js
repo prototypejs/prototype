d4b69e59273bef91c221ff479689800adc133e5b
Andrew Dupont <github@andrewdupont.net>
2014-05-10 13:40:47 -0500

    Fix issue where the deprecated Element#childOf was not initially present on elements.
    We added it to the Element.Methods object in deprecated.js _after_ the initial call to Element.addMethods, meaning that it didn't get copied over unless the user made a subsequent call to Element.addMethods for whatever reason. Unit tests didn't catch this because they call Element.addMethods before testing starts.

--- src/prototype/deprecated.js
+++ src/prototype/deprecated.js
@@ -4,7 +4,9 @@ Hash.toQueryString = Object.toQueryString;
 
 var Toggle = { display: Element.toggle };
 
-Element.Methods.childOf = Element.Methods.descendantOf;
+Element.addMethods({
+  childOf: Element.Methods.descendantOf
+});
 
 var Insertion = {
   Before: function(element, content) {
@@ -202,7 +204,7 @@ Object.extend(Element.ClassNames.prototype, Enumerable);
     initialize: function(expression) {
       this.expression = expression.strip();
     },
-  
+
     /** deprecated
      *  Selector#findElements(root) -> [Element...]
      *  - root (Element | document): A "scope" to search within. All results will
@@ -214,7 +216,7 @@ Object.extend(Element.ClassNames.prototype, Enumerable);
     findElements: function(rootElement) {
       return Prototype.Selector.select(this.expression, rootElement);
     },
-  
+
     /** deprecated
      *  Selector#match(element) -> Boolean
      *
@@ -223,11 +225,11 @@ Object.extend(Element.ClassNames.prototype, Enumerable);
     match: function(element) {
       return Prototype.Selector.match(element, this.expression);
     },
-  
+
     toString: function() {
       return this.expression;
     },
-  
+
     inspect: function() {
       return "#<Selector: " + this.expression + ">";
     }
@@ -244,7 +246,7 @@ Object.extend(Element.ClassNames.prototype, Enumerable);
     matchElements: function(elements, expression) {
       var match = Prototype.Selector.match,
           results = [];
-          
+
       for (var i = 0, length = elements.length; i < length; i++) {
         var element = elements[i];
         if (match(element, expression)) {
